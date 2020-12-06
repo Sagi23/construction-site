e23f9245fec8efdd3993cb4feb7dc7083e6db8c3
Sagi Twig <twigsagi@gmail.com>
2020-12-06 18:59:29 +0200

    Add bootsrap slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,4 @@
-const imgSlides = [];
-imgSlides[0] = "/static/slider2.jpg";
-imgSlides[1] = "/static/slider1.jpg";
-imgSlides[2] = "/static/slider3.jpg";
-const rightBtn = document.querySelector("#right-arrow");
-const leftBtn = document.querySelector("#left-arrow");
-const btn = document.querySelector("#btn");
-let imgSlider = document.querySelector(".slides");
-
-rightBtn.addEventListener("click", function () {
-  if (imgSlider.src.match(imgSlides[0])) {
-    imgSlider.src = imgSlides[1];
-  } else if (imgSlider.src.match(imgSlides[1])) {
-    imgSlider.src = imgSlides[2];
-  } else if (imgSlider.src.match(imgSlides[2])) {
-    imgSlider.src = imgSlides[0];
-  }
-});
-leftBtn.addEventListener("click", function () {
-  if (imgSlider.src.match(imgSlides[0])) {
-    imgSlider.src = imgSlides[2];
-  } else if (imgSlider.src.match(imgSlides[1])) {
-    imgSlider.src = imgSlides[0];
-  } else if (imgSlider.src.match(imgSlides[2])) {
-    imgSlider.src = imgSlides[1];
-  }
-});
+const btn = document.querySelector("#btn1");
 
 gsap.from(".landing-left", { duration: 0.5, x: -100, opacity: 0 });
 gsap.from(".landing-right", { duration: 0.5, x: 100, opacity: 0, delay: 0.5 });
